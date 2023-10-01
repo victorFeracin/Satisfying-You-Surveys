c8fc0206a310878389b2f8d6e910ee655d52c3c3
willian-onuki <willianonuki1@gmail.com>
2023-10-01 13:31:15 -0300

    feat: finished AcoesCard

--- src/components/AcoesCard/AcoesCard.js
+++ src/components/AcoesCard/AcoesCard.js
@@ -1,32 +1,44 @@
-import {
-  Text,
-  Image,
-  TouchableOpacity,
-  ScrollView
-} from 'react-native';
+import {Text, TouchableOpacity, ScrollView} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import StyleAcoesCard from './StyleAcoesCard';
+import {useNavigation} from '@react-navigation/native';
 
-const AcoesCard = (props) => {
-
+const AcoesCard = props => {
+  const navigation = useNavigation();
   return (
-    <ScrollView horizontal style={StyleAcoesCard.container}>
-      <TouchableOpacity style={StyleAcoesCard.containerCard} onPress={props.onPress}>
-        <Icon name="edit-document" size={50} color="#FFFFFF"/>
+    <ScrollView
+      horizontal
+      style={StyleAcoesCard.container}
+      contentContainerStyle={{
+        gap: 30,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+      showsHorizontalScrollIndicator={false}>
+      <TouchableOpacity
+        style={StyleAcoesCard.containerCard}
+        onPress={props.onPress}>
+        <Icon name="edit-document" size={50} color="#FFFFFF" />
         <Text style={StyleAcoesCard.title}>Modificar</Text>
       </TouchableOpacity>
 
-      <TouchableOpacity style={StyleAcoesCard.containerCard} onPress={props.onPress}>
-        <Icon name="library-add-check" size={50} color="#FFFFFF"/>
+      <TouchableOpacity
+        style={StyleAcoesCard.containerCard}
+        onPress={() =>
+          navigation.navigate('ColetarAcaoPesquisa', {card: props.card})
+        }>
+        <Icon name="library-add-check" size={50} color="#FFFFFF" />
         <Text style={StyleAcoesCard.title}>Coletar Dados</Text>
       </TouchableOpacity>
 
-      <TouchableOpacity style={StyleAcoesCard.containerCard} onPress={props.onPress}>
-        <Icon name="donut-large" size={50} color="#FFFFFF"/>
+      <TouchableOpacity
+        style={StyleAcoesCard.containerCard}
+        onPress={props.onPress}>
+        <Icon name="donut-large" size={50} color="#FFFFFF" />
         <Text style={StyleAcoesCard.title}>Relatório</Text>
       </TouchableOpacity>
     </ScrollView>
   );
-}
+};
 
 export default AcoesCard;
--- src/components/AcoesCard/StyleAcoesCard.js
+++ src/components/AcoesCard/StyleAcoesCard.js
@@ -1,8 +1,22 @@
 import { StyleSheet } from 'react-native';
 
 const StyleAcoesCard = StyleSheet.create({
-  
+  container: {
 
+  },
+  containerCard: {
+    padding: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#312464',
+    borderRadius: 10,
+  },
+  title: {
+    fontSize: 36,
+    fontFamily: 'AveriaLibre-Regular',
+    color: '#FFF',
+    marginTop: 14
+  }
 });
 
 export default StyleAcoesCard;
