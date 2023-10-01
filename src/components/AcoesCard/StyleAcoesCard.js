fc5e6900da800dca83aa1a25bcd5f086621c0a3c
Victor Hugo Feracin <victorhugo.feracin@gmail.com>
2023-10-01 17:58:25 -0300

    Adjusting cards on AcoesPesquisa

--- src/components/AcoesCard/StyleAcoesCard.js
+++ src/components/AcoesCard/StyleAcoesCard.js
@@ -3,14 +3,16 @@ import {StyleSheet} from 'react-native';
 const StyleAcoesCard = StyleSheet.create({
   container: {},
   containerCard: {
-    padding: 30,
+    padding: 15,
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: '#312464',
     borderRadius: 10,
+    width: 220,
+    height: 220
   },
   title: {
-    fontSize: 36,
+    fontSize: 24,
     fontFamily: 'AveriaLibre-Regular',
     color: '#FFF',
     marginTop: 14,
